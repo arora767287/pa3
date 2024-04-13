399ec9ace5180381a8aba3dcf5fa70d8363efe5d
Sachitt Arora <sachittarora@Sachitts-MacBook-Pro.local>
2024-04-13 00:25:25 -0500

    Fixing bugs

--- sparse.cpp
+++ sparse.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 struct Point {
     int row;
@@ -12,15 +13,16 @@ struct Point {
 };
 
 // using tuples of length 3 to represent sparse matrices
+#include <vector>
+
 std::vector<Point> generate_sparse(int s, int N, int p, int rank) {
     std::vector<Point> gen_mat;
     int start_row = rank * (N / p);
     int end_row = (rank + 1) * (N / p);
     for (int c = 0; c < N; c++) {
         for (int r = start_row; r < end_row; r++) {
-            int rand_num = rand() % N;
-            if (rand_num < s * N) {
-                Point point = new Point(t.row, t.col, rand() % 10); //row, col, value
+            if (rand() % N < s * N) {
+                Point point = {r, c, rand() % 10}; //row, col, value
                 gen_mat.push_back(point);
             }
         }
@@ -28,21 +30,20 @@ std::vector<Point> generate_sparse(int s, int N, int p, int rank) {
     return gen_mat;
 }
 
-void transpose_matrix(std::vector<Tuple>& matrix, int N, int p) {
+void transpose_matrix(std::vector<Point>& matrix, int N, int p) {
     std::vector<int> sendcounts(p, 0); //sending number per proc
     std::vector<int> sdispls(p, 0); //displacement
 
     for (Point& p : matrix) {
-        int d = p.col / (N / p); //dest 
-        sendcounts[d]++;
+        sendcounts[p.col / (N / p)]++;
     }
 
     sdispls[0] = 0;
     for (int i = 1; i < p; i++) {
-        sdispls[i] = sdispls[i - 1] + sendcounts[i - 1];
+        sdispls[i] = sdispls[i - 1] + sendcounts[i - 1]; //running total for displacement per proc
     }
 
-    std::vector<Tuple> transposed_matrix(matrix.size()); //storage for transposed matrix
+    std::vector<Point> transposed_matrix(matrix.size()); //storage for transposed matrix
 
     // i think??
     MPI_Alltoallv(matrix.data(), sendcounts.data(), sdispls.data(), MPI_INT, transposed_matrix.data(), sendcounts.data(), sdispls.data(), MPI_INT, MPI_COMM_WORLD);
@@ -57,6 +58,8 @@ void transpose_matrix(std::vector<Tuple>& matrix, int N, int p) {
 }
 
 int main(int argc, char** argv) {
+    #include <mpi.h> // Include the MPI header file
+
     MPI_Init(&argc, &argv);
 
     int rank, p;
@@ -69,12 +72,12 @@ int main(int argc, char** argv) {
     int pf = std::stoi(argv[3]);  // printing flag
     std::string out_file = argv[4];  // Ofile name
 
-    std::vector<Tuple> A = generate_sparse(s, N, p, rank);
-    std::vector<Tuple> B = generate_sparse(s, N, p, rank);
+    std::vector<Point> A = generate_sparse(s, N, p, rank);
+    std::vector<Point> B = generate_sparse(s, N, p, rank);
 
     transpose_matrix(B, N, p);
 
-    C_size = N * N / p;
+    int C_size = N * N / p;
     int* C = new int[C_size];
     for (int i = 0; i < C_size; i++) {
         C[i] = 0;
